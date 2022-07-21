e3237c2caef83900a260d89da0aa72821d87e255
Rohit Patil <rahulhimesh09@gmail.com>
2022-07-21 17:23:34 +0530

    refactor: :recycle: fix route builder function

--- src/routes.ts
+++ src/routes.ts
@@ -23,43 +23,36 @@ import { createdSessionSchema } from "./schemas/session.schema";
 import { createdUserSchema } from "./schemas/user.schema";
 
 export default function (app: Express) {
-  app.get(
-    "/healthcheck",
-    (req: Request, res: Response) => {
-      return res.sendStatus(200);
-    },
-    app.post(
-      "/api/users",
-      validateRequest(createdUserSchema),
-      createUserHandler
-    ),
-    app.post(
-      "/api/sessions",
-      validateRequest(createdSessionSchema),
-      createUserSessionHandler
-    ),
-    app.get("/api/sessions", requireUser, getUserSessionHandler),
-    app.delete("/api/sessions", requireUser, deleteSessionHandler),
+  app.get("/healthcheck", (req: Request, res: Response) => {
+    return res.sendStatus(200);
+  });
+  app.post("/api/users", validateRequest(createdUserSchema), createUserHandler);
+  app.post(
+    "/api/sessions",
+    validateRequest(createdSessionSchema),
+    createUserSessionHandler
+  );
+  app.get("/api/sessions", requireUser, getUserSessionHandler);
+  app.delete("/api/sessions", requireUser, deleteSessionHandler);
 
-    app.get(
-      "/api/products/:productId",
-      validateRequest(getProductSchema),
-      getProductHandler
-    ),
-    app.post(
-      "/api/products",
-      [requireUser, validateRequest(createProductSchema)],
-      createProductHandler
-    ),
-    app.put(
-      "/api/products/:productId",
-      [requireUser, validateRequest(updateProductSchema)],
-      updateProductHandler
-    ),
-    app.delete(
-      "/api/products/:productId",
-      [requireUser, validateRequest(deleteProductSchema)],
-      deleteProductHandler
-    )
+  app.get(
+    "/api/products/:productId",
+    validateRequest(getProductSchema),
+    getProductHandler
+  );
+  app.post(
+    "/api/products",
+    [requireUser, validateRequest(createProductSchema)],
+    createProductHandler
+  );
+  app.put(
+    "/api/products/:productId",
+    [requireUser, validateRequest(updateProductSchema)],
+    updateProductHandler
+  );
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validateRequest(deleteProductSchema)],
+    deleteProductHandler
   );
 }
